97e94f0dc176fdda299b494286be067f7927a7d1
Wessam-Ahmed <wessam.ah@outlook.com>
2023-10-12 02:34:11 +0400

    Add form request classes

---
+++ app/Http/Requests/Api/V1/CheckAvailableSeatsRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAvailableSeatsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::guard('api')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'start_station' => 'required|exists:stations,id',
+            'end_station' => 'required|exists:stations,id',
+        ];
+    }
+}
---
+++ app/Http/Requests/Api/V1/StoreBookingRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use App\Rules\ValidSeatForTrip;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreBookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::guard('api')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'seat_id' => [
+                'required',
+                'exists:seats,id',
+                new ValidSeatForTrip($this->input('start_station'), $this->input('end_station'))
+            ],
+            'start_station' => 'required|exists:stations,id',
+            'end_station' => 'required|exists:stations,id'
+        ];
+    }
+}
